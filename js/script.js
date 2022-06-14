d5f8bfcc09bc5abb01810935c762c2059f6b8ba5
NikolskyE <103742684+NikolskyE@users.noreply.github.com>
2022-06-14 13:47:30 +0300

    first add files

---
+++ js/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    $('.gallery__inner').slick({
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                  dots: true,
+                  arrows: false
+    
+                }
+            }    
+        ]
+
+    });
+
+
+    //page up
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1000) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    //Smooth scroll 
+
+    $("a[href^='#']").click(function(){
+        var _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+   
+});
