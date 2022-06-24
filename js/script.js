7432a4b8ef62f27eaf889991b59c3b26b24719f3
NikolskyE <103742684+NikolskyE@users.noreply.github.com>
2022-06-24 22:58:33 +0300

    Add files via upload
    English version ready

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
     $('.gallery__inner').slick({
         speed: 1200,
-        // adaptiveHeight: true,
+        adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         responsive: [
             {
-                breakpoint: 768,
+                breakpoint: 769,
                 settings: {
                   dots: true,
                   arrows: false
@@ -36,5 +36,71 @@ $(document).ready(function(){
         return false;
     });
 
+
+    var containers;
+function initDrawers() {
+	// Get the containing elements
+	containers = document.querySelectorAll("#more");
+	setHeights();
+	wireUpTriggers();
+	window.addEventListener("resize", setHeights);
+}
+
+window.addEventListener("load", initDrawers);
+
+function setHeights() {
+	containers.forEach(container => {
+		// Get content
+		let content = container.querySelector("#content");
+		// Needed if this is being fired after a resize
+		content.removeAttribute("aria-hidden");
+		// Height of content to show/hide
+		let heightOfContent = content.getBoundingClientRect().height;
+		// Set a CSS custom property with the height of content
+		container.style.setProperty("--containerHeight", `${heightOfContent}px`);
+		// Once height is read and set
+		setTimeout(e => {
+			container.classList.add("height-is-set");
+			content.setAttribute("aria-hidden", "true");
+		}, 0);
+	});
+}
+
+function wireUpTriggers() {
+	containers.forEach(container => {
+		// Get each trigger element
+		let btn = container.querySelector("#trigger");
+		// Get content
+		let content = container.querySelector("#content");
+		btn.addEventListener("click", () => {
+			container.setAttribute(
+				"data-drawer-showing",
+				container.getAttribute("data-drawer-showing") === "true" ? "false" : "true"
+			);
+			content.setAttribute(
+				"aria-hidden",
+				content.getAttribute("aria-hidden") === "true" ? "false" : "true"
+			);
+		});
+	});
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+})
    
 });
